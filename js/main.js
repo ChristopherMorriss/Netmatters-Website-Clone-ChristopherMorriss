60f4873694e232deb71e71242086a3a335c49803
ChristopherMorriss <chrismorriss711@gmail.com>
2024-07-01 15:25:45 +0100

    I have added a slide out/mobile menu

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let hideOrShow=0; //Variable used to check what should happen after the menu is clicked
+                 //It's a boolean variable so it could be represented with true (1) and false (0) instead
+let slideOutMenu = document.querySelector('#hamburger-menu'); //Targets the span which contains the hamburger menu icon
+slideOutMenu.addEventListener('click',()=>{
+  if (hideOrShow===0){ //If hideOrShow variable =0 (is off) when clicked, display the menu 
+    hideOrShow +=1; //The hideOrShow variable is now on
+    $('#pop-up-menu').css('display','block'); //The menu is displayed in flexbox
+  }
+  else if (hideOrShow===1){ //If hideOrShow variable =1 (is on) when clicked, hide the menu
+    hideOrShow -=1; //The hideOrShow variable is now off
+    $('#pop-up-menu').css('display','none'); //The menu is no longer displayed 
+  }
+});

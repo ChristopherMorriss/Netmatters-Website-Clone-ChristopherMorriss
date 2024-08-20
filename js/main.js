792f5670cfb1de09bafa1e21c10070fc6d34fd50
ChristopherMorriss <chrismorriss711@gmail.com>
2024-08-20 10:05:37 +0100

    Fixed slide out menu functionality
    Still need to black out other items when slide out menu is out, however

--- js/main.js
+++ js/main.js
@@ -413,3 +413,16 @@ hover_partners13.addEventListener('mouseover',()=>{
 hover_partners13.addEventListener('mouseout',()=>{
   hover_partners13.style.display="none";
 });
+
+let hamburger_menu=document.querySelector('#hamburger-menu');
+let side_menu_toggle=0;
+hamburger_menu.addEventListener('click',()=>{
+  if (side_menu_toggle ===0){
+    $('#container').css('margin-left','-350px');
+    side_menu_toggle=1;
+  }
+  else if (side_menu_toggle ===1){
+    $('#container').css('margin-left','0');
+    side_menu_toggle=0;
+  }
+});

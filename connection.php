e621f5cedc218055bd385b739a896da6280e7b57
ChristopherMorriss <chrismorriss711@gmail.com>
2024-09-13 14:18:49 +0100

    Improved CSS styling and added database connection

--- connection.php
+++ connection.php
@@ -1,15 +1,4 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "netmatters_php";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully"; 
+$db = new PDO("mysql:host=localhost;dbname=netmatters_php;port=3306","root","");
+var_dump($db);
 ?>
